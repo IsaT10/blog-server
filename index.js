a9c917de557c88980c79ffd6cdd6e77e30a1e094
IsaT <rakiib010@gmail.com>
2023-11-05 14:06:02 +0600

    connet server with mongodb and get blogs from database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
@@ -7,8 +8,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//blogDb  g5ozt8dTGWj31h76
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfd97zi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const blogsCollection = client.db('blogDB').collection('blogs');
+
+async function run() {
+  try {
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('blog server is running');
+});
+
+app.get('/api/blogs', async (req, res) => {
+  const result = await blogsCollection.find().toArray();
+  res.send(result);
 });
 
 app.listen(port, () => {
